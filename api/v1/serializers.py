d8e538ccea2d8621f9aa538267d373a7643bf522
Ayan Isaev <ayanisaev15122008@gmail.com>
2026-04-11 19:11:31 +0600

    Hw-5m: Completed API with custom validation by Isaev Ayan

---
+++ api/v1/serializers.py
@@ -0,0 +1,26 @@
+﻿from rest_framework import serializers
+from api.models import Category, Product, Review
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+    def validate_name(self, value):
+        if len(value) < 2: raise serializers.ValidationError('Слишком короткое название!')
+        return value
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+    def validate_price(self, value):
+        if value <= 0: raise serializers.ValidationError('Цена должна быть больше нуля!')
+        return value
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+    def validate_stars(self, value):
+        if not (1 <= value <= 5): raise serializers.ValidationError('Оценка должна быть от 1 до 5!')
+        return value
---
+++ api/v1/views.py
@@ -0,0 +1,30 @@
+﻿from rest_framework import generics
+from api.models import Category, Product, Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+class CategoryListAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'
+
+class ProductListAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+
+class ReviewListAPIView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = 'id'
