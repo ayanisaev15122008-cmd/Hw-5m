97d081add697c78ec35e7f126acad25bdcee672e
Ayan Isaev <ayanisaev15122008@gmail.com>
2026-05-12 14:11:50 +0600

    fixed homework 1-3

---
+++ common/permissions.py
@@ -0,0 +1,8 @@
+﻿from rest_framework import permissions
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method == 'POST':
+            return False
+        return request.user and request.user.is_staff
---
+++ common/validators.py
@@ -0,0 +1,7 @@
+﻿from django.core.exceptions import ValidationError
+from datetime import date
+def validate_age(birthdate):
+    today = date.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    if age < 18:
+        raise ValidationError('Возраст должен быть не менее 18 лет.')
